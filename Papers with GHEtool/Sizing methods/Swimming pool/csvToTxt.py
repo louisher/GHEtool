dbc2c4701af8182726f5f7e18b75ba31c57d7e94
louishermans <louis.hermans@kuleuven.be>
2022-09-01 12:59:59 +0200

    20 year simulation of swimming pool

--- Papers with GHEtool/Sizing methods/Swimming pool/csvToTxt.py
+++ Papers with GHEtool/Sizing methods/Swimming pool/csvToTxt.py
@@ -3,9 +3,11 @@ import numpy as np
 
 data = pd.read_csv("swimming_pool_one_column.csv",comment='#',sep=";",header=None,)
 heat = data[0].tolist()
-#for i in range(len(heat)):
- #   heat[i] = heat[i]*1000
-time = [3600*i for i in range(8760)]
-full_data = np.stack([time, heat], axis=1)
+heat_20y=[]
+for i in range(20):
+    heat_20y  = heat_20y +heat
+
+time = [3600*i for i in range(20*8760)]
+full_data = np.stack([time, heat_20y], axis=1)
 
 np.savetxt('power.txt', full_data, delimiter="\t",fmt='%f')
