e7ba95191f37011b4b1b5b00bfb4ea91cb5c6bd0
louishermans <louis.hermans@kuleuven.be>
2022-09-01 17:56:47 +0200

    results for office and swimming pool

---
+++ Papers with GHEtool/Sizing methods/Swimming pool/Plots.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+mod = pd.read_csv("ModelicaResults/TavgFluid12.csv",comment='#',sep=",",skiprows=[1])
+GHE = pd.read_csv("exports/L4_temperature_profile.csv",comment='#',sep=",",header=None)
+
+
+plt.figure()
+plt.plot(mod["Time"],mod["TAvgFluid"]-273.15, color = "b",linewidth = 0.5,label="modelica")
+plt.plot(mod["Time"],GHE, color = "r",linewidth = 0.5,label="GHEtool")
+plt.legend()
+
+
+a = GHE[0] - mod["TAvgFluid"]  + 273.15
+print(a)
+
+plt.figure()
+plt.plot(mod["Time"],a, color = "b",linewidth = 0.5,label="difference")
+plt.show()
+
+
