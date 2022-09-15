8793d87bf98f512e367d4fe0dc52c3f00edc9b25
louishermans <louis.hermans@kuleuven.be>
2022-09-15 18:59:08 +0200

    updated results L3 auditorium

--- Papers with GHEtool/Sizing methods/Auditorium/Plots.py
+++ Papers with GHEtool/Sizing methods/Auditorium/Plots.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-T_ground_avg = 11.012905782657654
+T_ground_avg = 11.362081571028261
 
 # load data
-mod_Tconst = pd.read_csv("ModelicaResults/TavgFluid_L4_Tconst.csv", comment='#', sep=",", skiprows=[])
-mod = pd.read_csv("ModelicaResults/TavgFluid_L4.csv", comment='#', sep=",", skiprows=[])
-GHE = pd.read_csv("exports/L4_temperature_profile.csv", comment='#', sep=",", header=None)
-GHE_Tw = pd.read_csv("exports/Tb_L4_temperature_profile.csv", comment='#', sep=",", header=None)
-mod_Tw_Tconst = pd.read_csv("ModelicaResults/TAveBor_L4_Tconst.csv", comment='#', sep=",")
-mod_Tw = pd.read_csv("ModelicaResults/TAveBor_L4.csv", comment='#', sep=",")
+mod_Tconst = pd.read_csv("ModelicaResults/TavgFluid_L3_Tconst.csv", comment='#', sep=",", skiprows=[])
+mod = pd.read_csv("ModelicaResults/TavgFluid_L3.csv", comment='#', sep=",", skiprows=[])
+GHE = pd.read_csv("exports/L3_temperature_profile.csv", comment='#', sep=",", header=None)
+GHE_Tw = pd.read_csv("exports/Tb_L3_temperature_profile.csv", comment='#', sep=",", header=None)
+mod_Tw_Tconst = pd.read_csv("ModelicaResults/TAveBor_L3_Tconst.csv", comment='#', sep=",")
+mod_Tw = pd.read_csv("ModelicaResults/TAveBor_L3.csv", comment='#', sep=",")
 
 # convert GHE data to numpy
 GHE_Tw = np.array(GHE_Tw.iloc[:, 0])
